77a553ebc0ee7316b24ea44bb61a7718b7863bf2
cancu <juancruz.cancu@gmail.com>
2024-01-07 18:07:01 -0300

    base code for animation

---
+++ script.js
@@ -0,0 +1,37 @@
+// constanst
+const WORLD_HEIGHT = 30;
+const WORLD_WIDTH = 100;
+
+const worldElem = document.querySelector('[data-world]');
+
+
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+
+let lastTime;
+function update(time) {
+    const delta = time - lastTime;
+    console.log(time);
+    console.log(`DELTA △ ${delta}`);
+
+    lastTime = time;
+    // window.requestAnimationFrame(update)    
+}
+
+window.requestAnimationFrame(update)
+
+
+function setPixelToWorldScale() {
+   let worldToPixelScale;
+   
+   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH    
+   } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT   
+   }
+
+   worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+   worldElem.style.heigth = `${WORLD_HEIGHT * worldToPixelScale}px`
+}
+
+
